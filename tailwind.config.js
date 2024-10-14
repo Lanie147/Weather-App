1d51185e7003d7ddbb112617a9b37dc793b05f9a
Ross Lane <ross.lane1999@gmail.com>
2024-10-14 16:20:47 +0100

    Add more features to navbar add static content to hero add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/*.ejs",
-  ".public/*.js"],
+  ".public/*.js", "./views/partials/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'grow-shrink': {
+          '0%, 100%': { transform: 'scale(1)' }, // Initial size
+          '50%': { transform: 'scale(1.1)' },    // Grows to 1.1 times its size at 50%
+        },
+      },
+      animation: {
+        'slow-grow-shrink': 'grow-shrink 6s ease-in-out infinite', //animates over the course of 10 seconds
+        'slow-spin': 'spin 60s linear infinite', // Custom slow spin animation
+      },
+    },
   },
   plugins: [],
-}
-
+};
