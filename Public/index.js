8cdd6d38d34fe34941fe0fddfcaf7195733fd7d8
Ross Lane <ross.lane1999@gmail.com>
2024-10-15 20:59:58 +0100

    Add functionality to hero

--- Public/index.js
+++ Public/index.js
@@ -4,6 +4,27 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const authToken = "0e51510d053d293f793b1cdd93887cbc";
+// const today = new Date();
+// const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
+// const day = daysOfWeek[today.getDay()];
+// console.log(daysOfWeek);
+const locationNames = {
+    chandlersford:{
+        lat: 50.995323,
+        lon: -1.369236
+    },
+    donkeys:{
+        lat: 50.627220,
+        lon: -1.225843
+    },
+    gosport:{
+        lat: 50.790797,
+        lon: -1.138200
+    },
+}
+
+
 
 app.use(express.static("public"))
 app.use("/css",express.static("dist"))
@@ -11,11 +32,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
     try{
-        res.render("index.ejs");
+        const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${locationNames.chandlersford.lat}&lon=${locationNames.chandlersford.lon}&units=metric&exclude=minutely&appid=` + authToken)
+        const result = response.data;
+        res.render("index.ejs", {data: result});
     }
     catch (error) {
-        console.log(error);
+        console.error("Failed to make request:", error.message);
         res.status(500);
+        res.render("index.ejs", {
+            error: error.message,
+    });
+    }
+});
+
+app.post("/", async (req, res) => {
+    try{
+        const location = req.body.location;
+        console.log(location);
+        // const locationSelected = locationNames.donkeys.lat
+        const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${locationNames.chandlersford.lat}&lon=${locationNames.chandlersford.lon}&units=metric&exclude=minutely&appid=` + authToken)
+        const result = response.data;
+        res.render("index.ejs", {data: result});
+    }
+    catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.status(500);
+        res.render("index.ejs", {
+            error: error.message,
+    });
     }
 });
 
